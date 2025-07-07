a4150c742240b3ad65f79e85ee301d2473084bc1
KorotkovaKate <korotkovakate10@gmail.com>
2025-07-07 18:29:57 +0300

    added admin button and own templates

--- src/Pages/AllTemplatePage.vue
+++ src/Pages/AllTemplatePage.vue
@@ -8,9 +8,14 @@
         v-model="searchQuery"
     />
 
-    <button class="btn">
+    <router-link
+        to="/admin_panel"
+        class="btn"
+        v-if="userRole === 'Admin'">
       For admin
-    </button>
+    </router-link>
+
+
   </header>
   <div class="container all-templates">
     <section class="public-templates my-2">
@@ -30,10 +35,10 @@
       <div class="forms-list">
         <div
             class="form-card"
-            v-for="form in recentForms"
+            v-for="form in allTemplates"
             :key="form.id"
         >
-          <img :src="form.image" alt="form thumbnail" />
+          <img :src=blankImage alt="template image" />
           <div class="form-info">
             <p class="form-title">{{ form.title }}</p>
             <p class="form-date">{{ form.date }}</p>
@@ -55,14 +60,14 @@ const recentForms = ref([]) // ← пока вручную или заполни
 
 const token = sessionStorage.getItem('token');
 
-/**@typedef {Object} Template
+/**@typedef {Object} PublicTemplate
  @property {bigint} id
  @property {string} title
  @property {string} imageUlr
  @property {int} countOfLikes
  */
 
-/** @type {Template[]} */
+/** @type {PublicTemplate[]} */
 let templates = ref([]);
 
 const GetPublicTemplates = async () => {
@@ -76,5 +81,46 @@ const GetPublicTemplates = async () => {
   }
 }
 onMounted(GetPublicTemplates)
+
+
+/**@typedef {Object} AllTemplate
+ @property {bigint} id
+ @property {string} title
+ @property {string} imageUlr
+ @property {int} countOfLikes
+ @property {int} status
+ */
+
+/** @type {AllTemplate[]} */
+let allTemplates = ref([]);
+const GetMyTemplates = async () => {
+  try {
+    const response = await axios.post('https://localhost:7165/Template/GetTemplatesByUserId',{userId: Number(sessionStorage.getItem('userId'))})
+    allTemplates.value = JSON.parse(JSON.stringify(response.data.$values));
+    console.log(allTemplates)
+  }
+  catch (error) {
+    console.error('Not found templates', error)
+  }
+}
+onMounted(GetMyTemplates)
+
+function getUserRoleFromToken() {
+  const token = sessionStorage.getItem('token');
+  if (!token) return null;
+
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload.Role || null;
+  } catch {
+    return null;
+  }
+}
+const userRole = ref(null);
+
+onMounted(() => {
+  userRole.value = getUserRoleFromToken();
+});
+
 </script>
 
