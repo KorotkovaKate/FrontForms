f7ad1037ea8c74ec537bfad72ab76d82d645ca7f
KorotkovaKate <korotkovakate10@gmail.com>
2026-06-15 13:38:20 +0300

    feat: update router, add checker for access to pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import AllTemplates from '../Pages/AllTemplatePage.vue'
 import AllUsers from '../Pages/AdminPanelPage.vue'
 import CreateTemplate from '../Pages/CreateTemplatePage.vue'
 import Statistics from '../Pages/StatisticPage.vue'
+import FormFiller from '../Pages/FillTemplatePage.vue'
+import TemplatePreview from '../Pages/TemplatePreviewPage.vue'
 
 
 const routes = [
@@ -15,7 +17,7 @@ const routes = [
     {
         path: '/reg',
         name: 'Registration',
-        component: RegistrationView
+        component: RegistrationView,
     },
     {
         path: '/auth',
@@ -25,28 +27,60 @@ const routes = [
     {
         path: '/admin_panel',
         name: 'AdminPanel',
-        component: AllUsers
+        component: AllUsers,
+        meta: { requiresAuth: true, requiresAdmin: true }
     },
     {
         path: '/all_templates',
         name: 'AllTemplates',
-        component: AllTemplates
+        component: AllTemplates,
+        meta: { requiresAuth: true }
     },
     {
         path: '/create_template',
         name: 'CreateTemplate',
-        component: CreateTemplate
+        component: CreateTemplate,
+        meta: { requiresAuth: true }
     },
     {
         path: '/statistics',
         name: 'Statistics',
-        component: Statistics
+        component: Statistics,
+        meta: { requiresAuth: true }
     },
+    {
+        path: '/fill_form/:id',
+        name: 'FillForm',
+        component: FormFiller,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/preview_page/:id',
+        name: 'TemplatePreview',
+        component: TemplatePreview,
+        meta: { requiresAuth: true }
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const token = sessionStorage.getItem('token')
+    const userRole = sessionStorage.getItem('userRole')
+
+    if (to.meta['requiresAuth'] && !token) {
+        return next('/auth')
+    }
+
+    if (to.meta['requiresAdmin'] && userRole !== 'Admin')
+    {
+        return next('/all_templates')
+    }
+
+    next()
 })
 
 export default router
